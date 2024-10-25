d52b7910acdfef29c595c84687c6ca5598a5e00e
Stephen <stephen.a.joly@gmail.com>
2024-10-25 10:21:29 -0400

    Displaying critics score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let colour = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={colour} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
